d9ffb0d6062cc6641412e99b32e9a6d2d3845107
Hanu <hanuman.dasa@gmail.com>
2020-06-20 01:28:17 -0300

    Updates

---
+++ precache-manifest.54490bee39c24e827f05b0ed04213daf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "554cc2e5434cdca209f83f2509c7066c",
+    "url": "/olirena/index.html"
+  },
+  {
+    "revision": "e7bbd3f78859d79f9164",
+    "url": "/olirena/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f654e0d24a9f26685308",
+    "url": "/olirena/static/js/2.582a3eca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/olirena/static/js/2.582a3eca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7bbd3f78859d79f9164",
+    "url": "/olirena/static/js/main.91161fcc.chunk.js"
+  },
+  {
+    "revision": "bd9efab61b8d86f909d7",
+    "url": "/olirena/static/js/runtime-main.8d2bd0c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/olirena/static/media/logo.5d5d9eef.svg"
+  }
+]);
